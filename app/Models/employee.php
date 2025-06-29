e4c0754465e6457b795eb50022a172889a67b131
Abuabdellah <Abuabdellah1099@gmail.com>
2025-06-29 23:50:52 +0300

    Initial commit with current changes

--- app/Models/employee.php
+++ app/Models/employee.php
@@ -1,11 +1,1 @@
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Employee extends Model
-{
-  
-    //
-}
+ 
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TeacherController;
-
+use App\Http\Controllers\EmployeeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,72 +14,37 @@ Route::get('/', function () {
 // Grouped routes for students and teachers
 Route::prefix('details')->group(function () {
     Route::get('students', function () {
-        return("List of students");
+        return "List of students";
     })->name('students');
 
     Route::get('teachers', function () {
-        return ("List of teachers and their details");
+        return "List of teachers and their details";
     })->name('teachers');
 });
 
-// Route parameters
-Route::get('student/{id}/{rollno}', function ($id, $rollno) {
-    return "Student ID: {$id} and Roll No: {$rollno}";
-});
-
-// fallback route
-Route::fallback(function () {
-    return "Page not found";
-});
-
-// Route name
+// Route parameters with name
 Route::get('student/{id}/{rollno}', function ($id, $rollno) {
     return "Student ID: {$id} and Roll No: {$rollno}";
 })->name('student.show');
 
-// view route
-Route::get('contactus/{name}/{email}', function ($name,$email) {
-    // $name = "Eibrahim";
-    // $email = "Eibrahim@gmail.com";
-    // return view('contactus', compact('name', 'email'));
-    // Method 1: Using with() method
+// View route with parameters
+Route::get('contactus/{name}/{email}', function ($name, $email) {
     return view('contactus')->with([
         'name' => $name,
         'email' => $email
     ]);
-
-    // Method 2: Using array as second parameter
-    return view('contactus', [
-        'name' => $name,
-        'email' => $email
-    ]);
-
-    // Method 3: Using compact() function
-    return view('contactus', compact('name', 'email'));
-
-
-
 });
 
-// route view
+// Simple view route
 Route::view('contactus', 'contactus', ['name' => 'name', 'email' => 'email']);
 
-
-// controller route
-// without grouping
-// Route::get('student1', [StudentController::class, 'index']);
-// Route::get('teacher', [StudentController::class, 'teacher']);
-// with grouping
 // Group all student controller routes
 Route::controller(StudentController::class)->group(function () {
-    // Route to display student index page
     Route::get('student1', 'index');
-    // Route to display teacher page
     Route::get('teacher', 'teacher');
-    // Route to display student details
 });
 
-// with param of teacher transfer  to  controller
+// Teacher route with parameters
 Route::get('teacher/{id}/{name}', [StudentController::class, 'teacher']);
 
 // Resource route for products
@@ -91,16 +55,16 @@ Route::get('/home', HomeController::class);
 
 Route::get('/teacher', [TeacherController::class, 'index']);
 
-use App\Http\Controllers\EmployeeController;
-
+// Employee routes
 Route::get('/employee', [EmployeeController::class, 'index']);
-// Route::post('/addEmployee', [EmployeeController::class, 'addEmployee']);
 Route::get('/addEmployee', [EmployeeController::class, 'addEmployee']);
 Route::get('/showEmployee/{id}', [EmployeeController::class, 'showEmployee']);
-// Route::put('/addEmployee', [EmployeeController::class, 'addEmployee']);
 Route::get('/updateEmployee/{id}', [EmployeeController::class, 'updateEmployee']);
-// delete route
-// Route::delete('/deleteEmployee/{id}', [EmployeeController::class, 'deleteEmployee']);
 Route::get('/deleteEmployee/{id}', [EmployeeController::class, 'deleteEmployee']);
+
+// Fallback route
+Route::fallback(function () {
+    return "Page not found";
+});
 
 
